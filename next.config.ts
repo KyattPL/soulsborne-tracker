c7a712e311e68e1b8d2a5fa8cd5c840c25aece7e
Kajetan Pynka <siema1234@gazeta.pl>
2025-02-10 02:12:55 +0100

    Idk why out doesnt exist

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/soulsborne-tracker'
+  basePath: '/soulsborne-tracker',
+  reactStrictMode: true
 };
 
 export default nextConfig;
