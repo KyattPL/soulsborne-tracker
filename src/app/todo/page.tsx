b51a7a763fdc3c76e4435c5d447427cd904c5f8c
Kajetan Pynka <siema1234@gazeta.pl>
2025-03-12 18:43:57 +0100

    Made fully complete TODO list

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -6,8 +6,22 @@ export default function Todo() {
                     Overall:
                 </h2>
                 <ul className="list-disc list-inside text-2xl">
-                    <li>More game-specific trackers like Ashes of War, Blood gems etc.</li>
+                    <li>Covenants ? (which one is active)</li>
+                    <li>Gathered items (A LOT of work for that, that includes spells/weapons/rings/items etc.)</li>
+                    <li>NPCs met / missions</li>
+                    <li>Bonfire / lantern / archstone tracking</li>
+                    <li>Worn rings</li>
                     <li>Twitch commands guide</li>
+                    <li>Twitch command to get the shareable link</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-6xl my-4">
+                    Demon&apos;s Souls:
+                </h2>
+                <ul className="list-disc list-inside text-2xl">
+                    <li>World tendency</li>
+                    <li>Character tendency (maybe displayed prettier idk)</li>
                 </ul>
             </div>
             <div>
@@ -16,6 +30,8 @@ export default function Todo() {
                 </h2>
                 <ul className="list-disc list-inside text-2xl">
                     <li>Handle chalice dungeons</li>
+                    <li>Track blood gems</li>
+                    <li>Caryll runes (similar to rings in souls)</li>
                 </ul>
             </div>
             <div>
@@ -24,6 +40,8 @@ export default function Todo() {
                 </h2>
                 <ul className="list-disc list-inside text-2xl">
                     <li>Handle all bosses (many minor ones)</li>
+                    <li>Track Ashes of War</li>
+                    <li>EASY: add maxSpiritAshUpgrade (works just like weapons upgrades up to +10)</li>
                 </ul>
             </div>
         </div>
