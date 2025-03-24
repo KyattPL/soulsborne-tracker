6db7f94002b864bbf909e469062b53e84095335d
Kajetan Pynka <siema1234@gazeta.pl>
2025-03-24 17:06:16 +0100

    Added structure for ALL eq items

--- src/data/equipmentItems.ts
+++ src/data/equipmentItems.ts
@@ -2,6 +2,8 @@ import { DES_EQ_ITEMS } from "./equipment/des_eq";
 import { DS1_EQ_ITEMS } from "./equipment/ds1_eq";
 import { DS2_EQ_ITEMS } from "./equipment/ds2_eq";
 import { DS3_EQ_ITEMS } from "./equipment/ds3_eq";
+import { BB_EQ_ITEMS } from "./equipment/bb_eq";
+import { ELDEN_EQ_ITEMS } from "./equipment/elden_eq";
 
 export interface EquipmentItem {
     id: string;
@@ -9,16 +11,6 @@ export interface EquipmentItem {
     category: 'covenant' | 'weapon' | 'firearm' | 'rune' | 'armor' | 'consumables' | 'arrows' | 'bolts' | 'rings' | 'spells' | 'talisman' | 'spiritAsh';
     optionalKey?: 'armorHead' | 'armorChest' | 'armorArms' | 'armorLegs';
 }
-
-const BB_EQ_ITEMS: Array<EquipmentItem> = [
-    { id: 'homeward-bone', name: 'Homeward bone', category: 'consumables' },
-    { id: 'crown-of-dusk', name: 'Crown of Dusk', category: 'armor', optionalKey: 'armorHead' }
-];
-
-const ELDEN_EQ_ITEMS: Array<EquipmentItem> = [
-    { id: 'homeward-bone', name: 'Homeward bone', category: 'consumables' },
-    { id: 'crown-of-dusk', name: 'Crown of Dusk', category: 'armor', optionalKey: 'armorHead' }
-];
 
 export const items: Record<string, Array<EquipmentItem>> = {
     'ds1': DS1_EQ_ITEMS,
